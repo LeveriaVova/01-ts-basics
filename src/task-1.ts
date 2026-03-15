9aba4e30d3f9c75bfbbb5f94f07cab515bf75d84
Leveria Volodymyr <leverya.vova123@gmail.com>
2026-03-15 17:44:01 +0200

    Initial commit

---
+++ src/task-1.ts
@@ -0,0 +1,11 @@
+// У цьому фрагменті коду оголошені три змінні з різними типами значень: текст, число і булеве значення.
+
+
+
+const name: string = "Alice";
+const age: number = 30;
+const isOnline: boolean = true;
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Online: ${isOnline}`);
